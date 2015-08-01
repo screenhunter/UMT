ea04bd93055133d7337c3af39ded876622927c24
Rajat <khanna.rjt@gmail.com>
2015-08-01 14:35:51 -0400

    Step 2.3.1.2 Completed

---
+++ UserManagement/war/guest/guest-app.js
@@ -0,0 +1,12 @@
+
+var DEBUG = true;
+
+var guestModule = angular.module('guestApp', []);
+
+guestModule.controller('SubscribeController', ['$http', '$scope', '$window', 
+	function($http, $scope, $window) {
+	
+		
+	
+	}
+]);
